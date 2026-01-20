d86c582edc7727118c81e48bf51071eb81736f6b
Arqam Javed <mohammadarqam@gmail.com>
2026-01-20 19:17:54 +0500

    Initial Commit

---
+++ Clarabot/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Clarabot
+//
+//  Created by Arqam´s on 1/20/26.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
